a54a12225f6e6df5274f81d7ecddeaa4c97a0030
nakario <nakario.jp@gmail.com>
2017-09-13 16:51:10 +0900

    Implement main function

--- segnmt/__main__.py
+++ segnmt/__main__.py
@@ -2,10 +2,28 @@
 module main
 """
 
+import argparse
+from importlib import import_module
+from pkgutil import iter_modules
+from typing import List
+from typing import Optional
 
-def main():
-    """main function"""
-    pass
+import segnmt.commands
+
+
+def main(arguments: Optional[List[str]] = None):
+    """Define command line parser and run the specified command."""
+    parser = argparse.ArgumentParser(prog='segnmt')
+    subparsers = parser.add_subparsers(dest='command')
+    commands = segnmt.commands
+    for _, command, is_pkg in iter_modules(commands.__path__):
+        assert not is_pkg
+        command_parser = subparsers.add_parser(command)
+        command_module = import_module(f"{commands.__name__}.{command}")
+        command_module.define_parser(command_parser)
+        command_parser.set_defaults(run=command_module.run)
+    args = parser.parse_args(args=arguments)
+    args.run(args)
 
 
 if __name__ == '__main__':
---
+++ segnmt/commands/__init__.py
@@ -0,0 +1,17 @@
+"""
+package commands
+
+Package commands has subcommands modules.
+They must be have the following functions.
+
+
+def define_parser(parser):
+    '''Define command specific options.'''
+    parser.add_argument(...)
+
+
+def run(args):
+    '''Run the command.'''
+    ...
+
+"""
