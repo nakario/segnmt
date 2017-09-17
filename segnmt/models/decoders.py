2d703818db365c667baccc4d7fc047fe5381b9f2
Unknown <nakario.jp@gmail.com>
2017-09-18 00:31:38 +0900

    Correct a misleading name

--- segnmt/models/decoders.py
+++ segnmt/models/decoders.py
@@ -10,20 +10,20 @@ class Decoder(chainer.Chain):
                  word_embeddings_size: int,
                  hidden_layer_size: int,
                  attention_hidden_layer_size: int,
-                 encoder_hidden_layer_size: int,
+                 encoder_output_size: int,
                  maxout_layer_size: int,
                  maxout_pool_size: int = 2):
         super(Decoder, self).__init__()
         with self.init_scope():
             self.emb = L.EmbedID(vocabulary_size, word_embeddings_size)
-            self.rnn = L.LSTM(word_embeddings_size + encoder_hidden_layer_size,
+            self.rnn = L.LSTM(word_embeddings_size + encoder_output_size,
                               hidden_layer_size)
             self.maxout = L.Maxout(word_embeddings_size +
-                                   encoder_hidden_layer_size +
+                                   encoder_output_size +
                                    hidden_layer_size,
                                    maxout_layer_size,
                                    maxout_pool_size)
             self.linear = L.Linear(maxout_layer_size, vocabulary_size)
-            self.attention = AttentionModule(encoder_hidden_layer_size,
+            self.attention = AttentionModule(encoder_output_size,
                                              attention_hidden_layer_size,
                                              hidden_layer_size)
