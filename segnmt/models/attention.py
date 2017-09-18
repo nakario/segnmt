0aab3913ac7e51ec9a8b6eb4b055bf403cc905e2
Unknown <nakario.jp@gmail.com>
2017-09-18 14:27:19 +0900

    Correct a mistake

--- segnmt/models/attention.py
+++ segnmt/models/attention.py
@@ -4,12 +4,12 @@ import chainer.links as L
 
 class AttentionModule(chainer.Chain):
     def __init__(self,
-                 encoder_hidden_layer_size: int,
+                 encoder_output_size: int,
                  attention_layer_size: int,
                  decoder_hidden_layer_size: int):
         super(AttentionModule, self).__init__()
         with self.init_scope():
-            self.linear_h = L.Linear(encoder_hidden_layer_size,
+            self.linear_h = L.Linear(encoder_output_size,
                                      attention_layer_size)
             self.linear_s = L.Linear(decoder_hidden_layer_size,
                                      attention_layer_size,
