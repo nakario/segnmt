494122fec3ee5baeff613f80e96f83aa2f557ab8
Unknown <nakario.jp@gmail.com>
2017-09-18 14:04:58 +0900

    Unify variable names

--- segnmt/models/attention.py
+++ segnmt/models/attention.py
@@ -4,14 +4,14 @@ import chainer.links as L
 
 class AttentionModule(chainer.Chain):
     def __init__(self,
-                 input_hidden_layer_size: int,
+                 encoder_hidden_layer_size: int,
                  attention_layer_size: int,
-                 outpu_hidden_layer_size: int):
+                 decoder_hidden_layer_size: int):
         super(AttentionModule, self).__init__()
         with self.init_scope():
-            self.linear_h = L.Linear(input_hidden_layer_size,
+            self.linear_h = L.Linear(encoder_hidden_layer_size,
                                      attention_layer_size)
-            self.linear_s = L.Linear(outpu_hidden_layer_size,
+            self.linear_s = L.Linear(decoder_hidden_layer_size,
                                      attention_layer_size,
                                      nobias=True)
             self.linear_o = L.Linear(attention_layer_size,
