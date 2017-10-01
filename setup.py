9ac4697403b46f46cfcadc25e6674a3ac33bbd67
Unknown <nakario.jp@gmail.com>
2017-10-02 00:52:00 +0900

    Specify requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
+import sys
+
 from setuptools import find_packages
 from setuptools import setup
 
 
-setup(name='segnmt',
-      version='0.1.0',
-      packages=find_packages())
+if sys.version_info < (3, 6):
+    sys.exit('This program needs Python 3.6 or later version.')
+
+
+setup(
+    name='segnmt',
+    version='0.1.0',
+    packages=find_packages(),
+    requires=['chainer>=2.1.0', 'matplotlib', 'progressbar2']
+)
